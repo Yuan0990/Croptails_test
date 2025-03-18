705ef0cdde75d75a8632f27b7e8bc600d9901b15
Yuan_test <1534396397@qq.com>
2025-03-18 17:42:59 +0800

    小鸡导航

--- scenes/characters/npc_states/walk_state.gd
+++ scenes/characters/npc_states/walk_state.gd
@@ -1,6 +1,6 @@
 extends NodeState
 
-@export var character: CharacterBody2D
+@export var character: NonPlayableCharacter
 @export var animated_sprite_2d: AnimatedSprite2D
 @export var navigation_agent_2d: NavigationAgent2D
 
@@ -10,6 +10,8 @@ extends NodeState
 var speed:float
 
 func _ready() -> void:
+	navigation_agent_2d.velocity_computed.connect(on_safe_velocity_computed)
+	#空闲时用该方法
 	call_deferred("character_setup")
 
 #角色设置函数
@@ -27,22 +29,42 @@ func set_movement_target() -> void:
 	
 func _on_enter() -> void:
 	animated_sprite_2d.play("walk")
+	character.current_walk_cycle = 0
 
 func _on_process(_delta : float) -> void:
 	pass
 
 func _on_physics_process(_delta : float) -> void:
+	#如果小鸡已经到达目标点
+	if navigation_agent_2d.is_navigation_finished():
+		character.current_walk_cycle+=1
+		#重新生成目标点
+		set_movement_target()
+		return
+	
 	# 前往目标点
 	var target_position: Vector2 = navigation_agent_2d.get_next_path_position()
-	# 获取小鸡当前的朝向	
+	# 获取小鸡当前的朝向
 	var target_direction: Vector2 = character.global_position.direction_to(target_position)
 	# 翻转朝向
-	animated_sprite_2d.flip_h = target_direction.x<0
-	character.velocity = target_direction * speed
+	var velocity: Vector2 = target_direction * speed
+	
+	if navigation_agent_2d.avoidance_enabled:
+		animated_sprite_2d.flip_h = target_direction.x<0
+		navigation_agent_2d.velocity = velocity
+	else:
+		character.velocity = velocity
+		character.move_and_slide()
+	
+func on_safe_velocity_computed(safe_velocity: Vector2) -> void:
+	animated_sprite_2d.flip_h = safe_velocity.x<0
+	character.velocity = safe_velocity
 	character.move_and_slide()
 	
 func _on_next_transitions() -> void:
-	pass
+	if character.current_walk_cycle == character.walk_cycles:
+		character.velocity = Vector2.ZERO
+		transition.emit("Idle")
 	
 func _on_exit() -> void:
 	animated_sprite_2d.stop()
